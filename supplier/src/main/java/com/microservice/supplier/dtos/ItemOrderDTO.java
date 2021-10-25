49f01931be9eb609859b5d13aa37f75883b774fc
lucastome <flucastome@gmail.com>
2021-10-25 11:08:01 -0300

    task 003 - Create dtos classes

---
+++ supplier/src/main/java/com/microservice/supplier/dtos/ItemOrderDTO.java
@@ -0,0 +1,23 @@
+package com.microservice.supplier.dtos;
+
+public class ItemOrderDTO {
+
+    private Long id;
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ supplier/src/main/java/com/microservice/supplier/dtos/ReservationDTO.java
@@ -0,0 +1,23 @@
+package com.microservice.supplier.dtos;
+
+public class ReservationDTO {
+
+    public Integer idReservation;
+    public Integer preparationTime;
+
+    public Integer getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(Integer idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public Integer getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(Integer preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+}
